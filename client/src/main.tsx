8db460805260820dd6b548e87acc2afe224f5e87
zad33rot <dh1fb@mail.ru>
2026-06-08 01:32:51 +0300

    исправление пути в main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux' 
 import { RouterProvider } from 'react-router-dom' 
 import { store } from './store/store' 
-import { router } from '../src/store/router' 
+import { router } from '../src/router' 
 import './styles/index.css'
 
 const rootElement = document.getElementById('root')
