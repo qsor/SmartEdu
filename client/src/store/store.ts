1bded25c28a543483de75bb0802b7bd14ef7ec84
Yrn178 <rybckinandrey@gmail.com>
2026-06-10 01:37:40 +0300

    Добавил authSlice.ts и обновил store.ts

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-// заглушка от ошибки
-export const store = configureStore({
-  reducer: {},
-});
+import authReducer from './slices/authSlice.ts';
 
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
