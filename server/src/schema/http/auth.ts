98f9f45893e2fde9ea17e92c3789f336b2603408
Maxim <ladurove993@gmail.com>
2026-06-16 17:29:43 +0300

    фикс роутов

--- server/src/schema/http/auth.ts
+++ server/src/schema/http/auth.ts
@@ -1,13 +1,20 @@
 // {"email": "...", "password": "..."}
 export interface LoginRequestBody {
-  email: string
-  password: string
+    email: string
+    password: string
 }
 
 // {"firstName": "...", "lastName": "...", "email": "...", "password": "..."}
 // {"firstName": "...", "lastName": null,  "email": "...", "password": "..."}
-export interface RegisterRequestBody {
-  name: string
-  email: string
-  password: string
+// Либо просто name (в этом случае lastName определяется по пробелу):
+// {"name": "...", "email": "...", "password": "..."}
+export type RegisterRequestBody = {
+    firstName: string
+    lastName: string
+    email: string
+    password: string
+} | {
+    name: string
+    email: string
+    password: string
 }
